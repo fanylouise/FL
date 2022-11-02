b871238d6d7468d7f473917d8b41658bc0ce8889
fanylouise <gandra.fany@gmail.com>
2022-11-01 21:12:28 -0300

    Re-styling social media footer

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const logo = document.getElementsByClassName('navLogo');
+const header = document.getElementsByTagName('header')
+console.log(logo);
+
+function show(){
+  header.classList.t
+}
+logo.addEventListener('click', show)
