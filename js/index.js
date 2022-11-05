f4dc87c2ed0c2dd526c4356802ed3d0d469b811c
fanylouise <gandra.fany@gmail.com>
2022-11-05 17:14:39 -0300

    Removing redundancies

--- js/index.js
+++
@@ -1,8 +0,0 @@
-const logo = document.getElementsByClassName('navLogo');
-const header = document.getElementsByTagName('header')
-console.log(logo);
-
-function show(){
-  header.classList.t
-}
-logo.addEventListener('click', show)
