fa7143657eac5dbf3c34f180f62d9543ba9e2cdd
fanylouise <gandra.fany@gmail.com>
2023-05-15 19:52:15 -0300

    Header, home and about static form [not responsive]

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 
 let currentYear = new Date().getFullYear()
-let current = document.querySelector('.current').innerHTML = currentYear
+let current = document.querySelector('.current').innerHTML = currentYear;
+
+
+  let span = document.querySelector('skill');
+  let skills = ['FrontEnd', 'UX Design', 'Web Design'];
+  for(i=0; i<skills.length;i++){
+    setTimeout()
+    console.log(skills[i])
+  }
+ 
